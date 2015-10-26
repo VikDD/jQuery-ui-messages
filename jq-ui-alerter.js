9aa8b6aa8af47eef39dc5083b96317dcb6ba6d4b
VikDD <github@vikdd.ru>
2015-10-26 10:36:31 +0300

    Create jq-ui-alerter.js

---
+++ jq-ui-alerter.js
@@ -0,0 +1,20 @@
+function alerter(question, _title,_ok_name) {
+    _title = _title || 'Сообщение';
+	_ok_name=_ok_name||'Закрыть';
+    var defer = jQuery.Deferred();
+    jQuery('<div></div>').css('z-index','1025').html(question)
+    .dialog({autoOpen: true,modal: true,width: "auto",
+		minWidth:400,
+		title: _title,buttons: [{text:_ok_name,click: function() {
+                defer.resolve("true");
+                jQuery(this).dialog("close");
+            }}]/*,
+			create: function (event, ui) {
+				if(_title=='no-message')
+				{$(".ui-widget-header").hide();}
+			}*/
+			});
+	$('.ui-dialog').css('z-index','1024');
+	$('.ui-widget-overlay').css('z-index','1023');
+    return defer.promise();
+}
